c492519560192cfbc1b9ac876822577fa5e3baf5
Matheus0631 <matheusrodriguesdasilva310706@gmail.com>
2025-06-06 18:57:47 -0300

    Continuação do projeto

--- Projeto.py
+++ Projeto.py
@@ -1,9 +1,9 @@
 def inserir_notas():
-    num_notas = input("Digite a quantiddade de notas que vc deseja inserir:")
+    num_notas = int(input("Digite a quantidade de notas que vc deseja inserir:"))
     notas = [] #Vai receber as notas.
 
     for i in range(num_notas):                  #vai fazer um loop de acordo com a quantidade de notas desejadas pelo usuário.
-        nota = float(input(f"digite a {i + 1} nota:"))
+        nota = float(input(f"digite a {i + 1}ª nota:"))
         notas.append(nota)      #Vai adicionar o valor da variável nota na lista.
     return notas                #Vai retornar as notas.
 
@@ -11,7 +11,7 @@ def inserir_notas():
 
 def calcular_medias(notas):     
     if not notas:           #Vai verificar se a lista está vazia e retornar o erro.
-        return "ERRO:A lista etá vazia, é necessário que vc adicione alguma nota para poder calcular a média. "
+        return "ERRO:A lista está vazia, é necessário que você adicione alguma nota para poder calcular a média. "
     
     for nota in notas:      #Vai varificar na lista se tem alguma nota irregular e retornar o erro.
         if nota <0 or nota > 10:
@@ -19,3 +19,29 @@ def calcular_medias(notas):
     
 
     return sum(notas)/len(notas)            #vai somar as notas dentro da lista e dividir pela quantidade de notas dentro dela.
+
+
+        #Vai mostrar os resultados.
+def resultados(notas, media):
+    print("\n Notas inseridas:", notas)
+    if isinstance(media, str):
+        print(media)
+
+    else:
+        print(f"Média das notas {media:.2f}")
+
+
+def main():
+
+    print("Bem-Vindo ao calculador de médias!\n")
+
+    notas_alunos = inserir_notas()
+
+    media_notas = calcular_medias(notas_alunos)
+
+    resultados(notas_alunos,media_notas)
+
+
+if __name__ == "__main__":
+    main()  
+
